64ca7603c9f959a10dfdf1ec0338eacfacf9e3d3
Deniz <denizozcan9@gmail.com>
2023-05-30 20:01:29 -0400

    Historical data + current values
    Fixed minor issues
    Extended date range to today
    Fixed quantity calculations
    Added calculations for cash
    Added historical price data from yf
    Added calculations for portfolio value
    Ready to add other metrics and visualizations

--- Dashboard.py
+++ Dashboard.py
@@ -3,7 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import yfinance as yf
+from datetime import datetime, date, timedelta
 
 # Get the absolute path to the directory where the script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -27,7 +28,7 @@ transactions_raw.columns= transactions_raw.columns.str.replace(' ','_').str.lowe
 transactions = transactions_raw[['date', 'description', 'net_amount', 'security_id', 'quantity', 'price', 'commission/fees']]
 
 # Rename columns
-transactions.rename(columns={'security_id':'symbol', 'commission/fees':'fees'}, inplace=True)
+transactions = transactions.rename(columns={'security_id':'symbol', 'commission/fees':'fees'})
 
 # Define function to categorize transactions
 def categorize_transactions(row):
@@ -47,18 +48,20 @@ def categorize_transactions(row):
         return 'other'
 
 # Apply function to categorize transactions
-transactions['type'] = transactions.apply(categorize_transactions, axis=1)
+transactions.loc[:, 'type'] = transactions.apply(categorize_transactions, axis=1)
 
 # Separate transactions into symbol transactions and non-symbol transactions
 symbol_transactions = transactions[transactions['symbol'].notna()].copy()
 non_symbol_transactions = transactions[transactions['symbol'].isna()].copy()
 
 # Convert 'net_amount' to numeric
-symbol_transactions['net_amount'] = pd.to_numeric(symbol_transactions['net_amount'], errors='coerce')
-non_symbol_transactions['net_amount'] = pd.to_numeric(non_symbol_transactions['net_amount'], errors='coerce')
+symbol_transactions['net_amount'] = pd.to_numeric(symbol_transactions['net_amount'].str.replace(',', ''), errors='coerce')
+non_symbol_transactions['net_amount'] = pd.to_numeric(non_symbol_transactions['net_amount'].str.replace(',', ''), errors='coerce')
 
 # Set date range and create a dataframe with daily data for each symbol
-date_range = pd.date_range(start=symbol_transactions.date.min(), end=symbol_transactions.date.max())
+start_date = transactions.date.min()
+end_date = datetime.now().date()
+date_range = pd.date_range(start=start_date, end=end_date)
 symbols = symbol_transactions.symbol.unique()
 
 idx = pd.MultiIndex.from_product([symbols, date_range], names=['symbol', 'date'])
@@ -68,18 +71,22 @@ daily_data = pd.DataFrame(index=idx).reset_index()
 symbol_transactions = pd.merge(daily_data, symbol_transactions, on=['symbol', 'date'], how='left')
 symbol_transactions['symbol'] = symbol_transactions['symbol'].ffill()
 
-# Create masks for transaction types
+# Create masks for symbol transactions
 buy_mask = symbol_transactions['type'] == 'buy'
 sell_mask = symbol_transactions['type'] == 'sell'
 dividend_mask = symbol_transactions['type'] == 'dividend'
-deposit_mask = symbol_transactions['type'] == 'deposit'
-withdrawal_mask = symbol_transactions['type'] == 'withdrawal'
 
 # Make 'quantity' negative for 'sell' transactions
 symbol_transactions.loc[sell_mask, 'quantity'] = -symbol_transactions.loc[sell_mask, 'quantity']
 
+# Ensure transactions are sorted by date
+transactions = transactions.sort_values('date')
+
+# Fill NaN with 0 for quantity
+symbol_transactions['quantity'] = symbol_transactions['quantity'].fillna(0)
+
 # Calculate cumulative quantity for each symbol, forward fill and fill NaN with 0
-symbol_transactions['cumulative_quantity'] = symbol_transactions.groupby('symbol')['quantity'].cumsum().ffill().fillna(0)
+symbol_transactions['cumulative_quantity'] = symbol_transactions.groupby('symbol')['quantity'].cumsum().fillna(0)
 
 # Calculate dividends received for each symbol
 symbol_transactions.loc[dividend_mask, 'dividends'] = symbol_transactions.loc[dividend_mask, 'net_amount'].groupby(symbol_transactions['symbol']).cumsum()
@@ -100,7 +107,7 @@ symbol_transactions.loc[sell_mask, 'avg_sell_price'] = (symbol_transactions.loc[
 symbol_transactions['avg_sell_price'] = symbol_transactions.groupby('symbol')['avg_sell_price'].ffill()
 
 # Calculate total cost for each buy transaction
-symbol_transactions.loc[buy_mask, 'total_cost'] = symbol_transactions.loc[buy_mask, 'price'] * symbol_transactions.loc[buy_mask, 'quantity'] + symbol_transactions.loc[buy_mask, 'fees']
+symbol_transactions.loc[buy_mask, 'total_cost'] = symbol_transactions.loc[buy_mask, 'price'] * symbol_transactions.loc[buy_mask, 'quantity'] - symbol_transactions.loc[buy_mask, 'fees']
 
 # Calculate cumulative total cost for each symbol, forward fill and fill NaN with 0
 symbol_transactions['cumulative_total_cost'] = symbol_transactions.groupby('symbol')['total_cost'].cumsum().ffill().fillna(0)
@@ -119,7 +126,7 @@ symbol_transactions.loc[sell_mask_prev_day_quantity, 'avg_cost_basis_per_share']
 symbol_transactions['avg_cost_basis_per_share'] = symbol_transactions.groupby('symbol')['avg_cost_basis_per_share'].ffill()
 
 # Calculate sales proceeds net of fees for sell transactions
-symbol_transactions.loc[sell_mask, 'sales_proceeds'] = (symbol_transactions.loc[sell_mask, 'price'] * - symbol_transactions.loc[sell_mask, 'quantity']) - symbol_transactions.loc[sell_mask, 'fees']
+symbol_transactions.loc[sell_mask, 'sales_proceeds'] = (symbol_transactions.loc[sell_mask, 'price'] * - symbol_transactions.loc[sell_mask, 'quantity']) + symbol_transactions.loc[sell_mask, 'fees']
 
 # For each sell transaction, calculate cost basis of sold shares based on average cost basis per share at the time of sale
 symbol_transactions.loc[sell_mask, 'sell_cost_basis'] = symbol_transactions.loc[sell_mask, 'avg_cost_basis_per_share'] * -symbol_transactions.loc[sell_mask, 'quantity']
@@ -146,11 +153,83 @@ non_symbol_transactions['cumulative_total'] = non_symbol_transactions.groupby('t
 # Merge symbol and non-symbol transactions back together
 transactions = pd.concat([symbol_transactions, non_symbol_transactions], ignore_index=True)
 
+# Calculate cash flow for each transaction
+def calculate_cash(row):
+    if row['type'] == 'buy':
+        return -row['total_cost']
+    elif row['type'] == 'sell':
+        return row['sales_proceeds']
+    elif row['type'] == 'dividend':
+        return row['net_amount']
+    elif row['type'] == 'deposit':
+        return row['net_amount']
+    elif row['type'] == 'withdrawal':
+        return row['net_amount']
+    elif row['type'] == 'fee':
+        return row['net_amount']
+    else:
+        return 0
+
+transactions['cash'] = transactions.apply(calculate_cash, axis=1)
+
+# Ensure the transactions are sorted by date
+transactions = transactions.sort_values('date')
+
+# Compute cumulative cash value
+transactions['cumulative_cash'] = transactions['cash'].cumsum()
+
+# Load daily price data for each symbol in the portfolio
+symbols_list = symbols.tolist()
+prices = yf.download(symbols_list, start=start_date, end=end_date)['Adj Close']
+
+# Reset the index to create a column for the dates
+prices.reset_index(inplace=True)
+
+# Rename the 'Date' column to 'date' to match the transactions DataFrame
+prices.rename(columns={'Date': 'date'}, inplace=True)
+
+# Melt the prices DataFrame
+prices = prices.melt(id_vars='date', var_name='symbol', value_name='adj_close')
+
+# Merge the transactions and prices dataframes
+transactions = pd.merge(transactions, prices, how='left', on=['symbol', 'date'])
+
+# Forward fill the adjusted closing prices
+transactions['adj_close'] = transactions.groupby('symbol')['adj_close'].ffill()
+
+# Calculate daily values of each holding
+transactions['daily_value'] = transactions['cumulative_quantity'] * transactions['adj_close']
+
+# # Calculate daily portfolio value including cash
+# portfolio_value = transactions.groupby('date')['daily_value'].sum() + transactions.set_index('date')['cumulative_cash']
+
+# Calculate daily portfolio value including cash
+portfolio_value = transactions.groupby('date').apply(lambda x: x['daily_value'].sum() + x['cumulative_cash'].iloc[-1])
+
+# Calculate daily portfolio returns
+portfolio_returns = portfolio_value.pct_change().dropna()
+
+# Change column name to 'Portfolio Value ($)'
+portfolio_value = portfolio_value.rename('Portfolio Value ($)')
+
 # Create a summary DataFrame
 summary = transactions.groupby('symbol').last()
 
-# Show only the columns we're interested in
-summary = summary[['cumulative_quantity', 'realized_returns', 'dividends']]
+# Rename columns
+summary = summary.rename(columns={'symbol': 'Symbol', 'cumulative_quantity': 'Quantity', 'adj_close': 'Current Price', 'daily_value': 'Current Value',
+                        'realized_returns': 'Realized Return', 'dividends': 'Dividends Received'})
 
-# Display the summary DataFrame in Streamlit
+# Add the latest cash value to the summary DataFrame
+summary.loc['Cash', 'Current Value'] = transactions['cumulative_cash'].iloc[-1]
+
+# Show only the columns we're interested in
+summary = summary[['Quantity', 'Current Price', 'Current Value', 'Realized Return', 'Dividends Received']]
+
+summary = summary.round(2)
+
+# Create streamlit display elements
+st.title('Portfolio Dashboard')
+st.metric(label='Portfolio Value', value=portfolio_value.iloc[-1].round(2))
+st.area_chart(data = portfolio_value)
+
 st.dataframe(summary)
